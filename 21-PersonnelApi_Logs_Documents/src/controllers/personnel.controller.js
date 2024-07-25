993066bde33431a439d125fb38c564f4dfcc7ade
yksl <yukselkurtuldu9@gmail.com>
2024-07-25 23:07:30 +0300

    up

--- 21-PersonnelApi_Logs_Documents/src/controllers/personnel.controller.js
+++ 21-PersonnelApi_Logs_Documents/src/controllers/personnel.controller.js
@@ -7,6 +7,19 @@ const Personnel = require("../models/personnel.model");
 
 module.exports = {
   list: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "List Personnels"
+        #swagger.description = `
+            You can send query with endpoint for search[], sort[], page and limit.
+            <ul> Examples:
+                <li>URL/?<b>filter[field1]=value1&search[field2]=value2</b></li>
+                <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                <li>URL/?<b>page=2&limit=1</b></li>
+            </ul>
+        `
+    */
     const data = await res.getModelList(Personnel);
 
     res.status(200).send({
@@ -17,6 +30,19 @@ module.exports = {
   },
 
   create: async (req, res) => {
+/*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Create Personnel"
+        #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Department'
+                }
+            }
+    */
+
+
     // const isFirstAccount = (await Personnel.countDocuments()) === 0;
     // req.body.isAdmin = isFirstAccount ? true : false;
 
@@ -38,6 +64,10 @@ module.exports = {
   },
 
   read: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Get Single Personnel"
+    */
     const data = await Personnel.findOne({ _id: req.params.id });
 
     res.status(200).send({
@@ -47,6 +77,18 @@ module.exports = {
   },
 
   update: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Update Personnel"
+        #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Department'
+                }
+            }
+    */
+
     // isLead Control:
     const isLead = req.body?.isLead || false;
     if (isLead) {
@@ -72,6 +114,10 @@ module.exports = {
   },
   //Ödev:team lead silinirse
   delete: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Delete Personnel"
+    */
     const data = await Personnel.deleteOne({ _id: req.params.id });
 
     res.status(data.deletedCount ? 204 : 404).send({
--- 21-PersonnelApi_Logs_Documents/swaggerAutogen.js
+++ 21-PersonnelApi_Logs_Documents/swaggerAutogen.js
@@ -12,7 +12,7 @@
 // https://swagger-autogen.github.io/docs/
 
 require('dotenv').config()
-const HOST = process.env.HOST || 'http://127.0.0.1'
+const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 8000
 
 
