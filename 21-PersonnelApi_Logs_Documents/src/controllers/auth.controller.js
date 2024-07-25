d7928c642525e1b5d5a02e367f1e61bb4bcb139f
yksl <yukselkurtuldu9@gmail.com>
2024-07-25 22:45:56 +0300

    up

--- 21-PersonnelApi_Logs_Documents/src/controllers/auth.controller.js
+++ 21-PersonnelApi_Logs_Documents/src/controllers/auth.controller.js
@@ -14,6 +14,20 @@ module.exports = {
 
     login: async (req, res) => {
 
+        /*
+            #swagger.tags = ['Authentication']
+            #swagger.summary = 'Login'
+            #swagger.description = 'Login with username and password'
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    username: '*String',
+                    password: '*String',
+                }
+            }
+        */
+
         const { username, password } = req.body
 
         if (username && password) {
@@ -58,6 +72,12 @@ module.exports = {
     },
 
     logout: async (req, res) => {
+
+        /*
+            #swagger.tags = ['Authentication']
+            #swagger.summary = 'Logout'
+            #swagger.description = 'Delete token.'
+        */
 
         // Token Delete:
         // if (req.user) {
