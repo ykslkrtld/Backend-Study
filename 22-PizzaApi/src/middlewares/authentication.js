05ac71bafbb77ba57b695252288690c3bde67a64
yksl <yukselkurtuldu9@gmail.com>
2024-07-31 22:28:15 +0300

    up

--- 22-PizzaApi/src/middlewares/authentication.js
+++ 22-PizzaApi/src/middlewares/authentication.js
@@ -5,33 +5,18 @@
 
 const Token = require("../models/token");
 
-const jwt = require('jsonwebtoken')
-
 module.exports = async (req, res, next) => {
+  const auth = req.headers?.authorization; // Token ...tokenKey...
+  const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
 
-    req.user = null
-
-    const auth = req.headers?.authorization; // Token ...tokenKey... || Bearer ...jwtAccess...
-    const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...'] || ['Bearer', '...jwtAccess...']
-
-    if (tokenKey) {
-
-        if (tokenKey[0] == "Token") { // SIMPLE TOKEN
-
-            const tokenData = await Token.findOne({ token: tokenKey[1] }).populate("userId");
-            req.user = tokenData ? tokenData.userId : false;
-
-        } else if (tokenKey[0] == "Bearer") { // JWT
-
-            jwt.verify(tokenKey[1], process.env.ACCESS_KEY, function (error, accessData) {
-
-                // console.log(accessData)
-                req.user = accessData ? accessData : null
-
-            })
-
-        }
+  if (tokenKey) {
+    if (tokenKey[0] == "Token") {
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId",
+      );
+      req.user = tokenData ? tokenData.userId : false;
     }
-    next();
+  }
+  next();
 };
 
