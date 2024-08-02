506e0ef7cd90afff99c065c97cc1e27382acbc90
yksl <yukselkurtuldu9@gmail.com>
2024-08-02 17:26:01 +0300

    up

--- 24-PizzaApi_emailAndUpload/src/routes/pizza.js
+++ 24-PizzaApi_emailAndUpload/src/routes/pizza.js
@@ -3,6 +3,30 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 const router = require("express").Router();
+
+/* ------------------------------------------------------- */
+// UPLOAD (Multer Middleware)
+// npm i multer
+// https://expressjs.com/en/resources/middleware/multer.html
+
+const multer = require('multer')
+
+const upload = multer({
+    // dest: './uploads', // yerine storage kullanarak birden fazla ayar yapılabilir. örn isimlendirme
+    storage: multer.diskStorage({
+        destination: './uploads',
+        filename: function (req, file, returnCallback) {
+            // console.log('file', file)
+            // returnCallback(error, fileName)
+            // returnCallback(null, 'yksl.jpg') // istediğin isim
+            // returnCallback(null, file.originalname) // orjinal ismi
+            returnCallback(null, Date.now() + '_' + file.originalname) // aynı isimler karışmaması / dosya üzerine yazmaması için
+        }
+    })
+})
+
+// const upload = require('../middlewares/upload')
+
 /* ------------------------------------------------------- */
 // routes/pizza:
 
@@ -10,13 +34,24 @@ const pizza = require("../controllers/pizza");
 
 // URL: /pizzas
 
-router.route("/").get(pizza.list).post(pizza.create);
+router.route("/")
+  .get(pizza.list)
+  // .post(upload.single('image'), pizza.create);
+  .post(upload.array('image'), pizza.create);
+  // .post(upload.any(), pizza.create);
+  // gönderirken image ismi ile gödereceksin daha doğrusu orada belirtilen isim neyse. any ile isim vermeden istenildiği kadar gönderilebilir fakat isim belirleyerek daha güvenli olmasını sağlıyoruz
+
+  /*
+    <form action="/pizzas" method="POST" enctype="mutipart/form-data">
+        <input type="file" name="image">
+    </form>
+  */ 
 
 router
   .route("/:id")
   .get(pizza.read)
-  .put(pizza.update)
-  .patch(pizza.update)
+  .put(upload.single('image'), pizza.update)
+  .patch(upload.single('image'), pizza.update)
   .delete(pizza.delete);
 
 /* ------------------------------------------------------- */
